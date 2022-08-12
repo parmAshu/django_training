38538b85e3d6e9ad7adaa2eec1431cdf7f8cdfd5
Ashutosh Singh Parmar <51224274+parmAshu@users.noreply.github.com>
2022-08-12 19:52:13 +0530

    Added blog website

---
+++ blogSite/blogSite/views.py
@@ -0,0 +1,30 @@
+import json
+
+from django.http import HttpResponse
+from django.shortcuts import render
+
+from pathlib import Path
+
+# Build paths inside the project like this: BASE_DIR / 'subdir'.
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+def homePage( request ):
+    context = { "blogList" : [] }
+    with open( BASE_DIR / "blogs/blogs.json" ) as file:
+        blogs = json.loads( file.read() )
+        for blog in blogs:
+            del( blog["Content"] )
+            blog[ "urlRelative" ] = "/blog?title=" + blog["Title"]
+            context["blogList"].append( blog )
+
+    return render( request, 'homePage.html', context=context )
+
+def blogPage( request ):
+    blogPath = request.GET.get( "title" )
+    context = {}
+    with open( BASE_DIR / "blogs/blogs.json" ) as file:
+        blogs = json.loads( file.read() )
+        for blog in blogs:
+            if blog["Title"] == blogPath:
+                context = blog
+    return render( request, 'blog.html', context = context )
